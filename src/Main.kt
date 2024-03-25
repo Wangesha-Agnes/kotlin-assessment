96f6979665654ab848660acff1d93178666c823c
Nancy-wainaina <wainainanancywambui@gmail.com>
2024-03-25 08:32:24 +0300

    calculations

---
+++ src/Main.kt
@@ -0,0 +1,30 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+
+}
+
+fun sentence(word:String){
+    var sentence = "Barnie bakes brown bagels and burns"
+
+
+}
+//Write and invoke one function that takes in an array of integers and returns
+//these 3 values: sum, count and average of all the elements.
+fun arrayOfNumbers(numbers:Int) {
+    var Numbers=(arrayOf(20, 30, 40, 50, 60))
+    println(numbers.sum())
+    println(numbers.count)
+    println(numbers.size)
+    println((numbers.sum() / numbers.size.toDouble())
+    println(numbers.average())
+}
+
+fun volume (numbers:Int){
+//var volume = 4/3*3.14159
+}
+    fun isPalindrome(word:String):Boolean{
+     if(word==)
+
+     }
